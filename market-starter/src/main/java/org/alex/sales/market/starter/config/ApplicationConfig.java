ae5a7ce45f3dc3dafa8b3f1295fa99053e27761b
kimp07@gmail.com <kimp07@gmail.com>
2023-04-09 11:37:51 +0300

    Project Market application started

---
+++ market-starter/src/main/java/org/alex/sales/market/starter/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package org.alex.sales.market.starter.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"org.alex.sales.market.*"})
+public class ApplicationConfig {
+}
